518ad648256c6b07debb40134e9441dd6c886af2
pxdrobdf <phladev05@gmail.com>
2024-06-17 14:23:19 -0300

    feat: add button component

---
+++ components/button/Button.tsx
@@ -0,0 +1,50 @@
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+interface ButtonProps {
+  text: string,
+  variant: 'primary' | 'outline',
+  onPress: () => void
+}
+
+const Button = ({text, variant, onPress}: ButtonProps) => {
+  const getStyle = () => {
+    switch (variant) {
+      case 'primary':
+        return [styles.primary]
+      case 'outline':
+        return [styles.outline]
+      default:
+        return [styles.primary]
+    }
+  }
+
+  return ( 
+    <TouchableOpacity style={getStyle()} onPress={onPress}>
+      <Text style={{color: '#fff', fontSize: 16}}>{text}</Text>
+    </TouchableOpacity>
+   );
+}
+
+const styles = StyleSheet.create({
+  primary: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+
+    backgroundColor: '#D22525',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    borderRadius: 5,
+  },
+  outline: {
+    backgroundColor: '#2C2C2C',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    borderRadius: 5,
+    borderStyle: "solid",
+    borderColor: 'black',
+    borderWidth: 1
+  },
+})
+ 
+export default Button;
