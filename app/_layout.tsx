8f3f18149f0f5ac855c0fd1e61288b9581973c96
pxdrobdf <phladev05@gmail.com>
2024-06-20 22:26:28 -0300

    feat: add header to login and register page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,18 @@ export default function RootLayout() {
   return(
     <Stack>
       <Stack.Screen name='index' options={{headerShown: false}}/>
-      <Stack.Screen name='(auth)/login'  options={{headerShown: false}}/>
-      <Stack.Screen name='(auth)/register' options={{headerShown: false}}/>
+      <Stack.Screen name='(auth)/login'  options={{
+        headerShown: true, 
+        title: 'Login', 
+        headerStyle: {backgroundColor: '#2c2c2c',}, 
+        headerTitleStyle: {color: '#ffff'}
+        }}/>
+      <Stack.Screen name='(auth)/register' options={{
+        headerShown: true,
+        title: 'Registro', 
+        headerStyle: {backgroundColor: '#2c2c2c'}, 
+        headerTitleStyle: {color: '#ffff'}, 
+        }}/>
       <Stack.Screen name='(tabs)' options={{headerShown: false}}/>
     </Stack>
   )
